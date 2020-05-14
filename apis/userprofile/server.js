86ba9d8465196921785890e13d5d3262c8928471
Daniel Heinze <daheinze@microsoft.com>
2020-05-14 12:50:49 +0200

    Update server.js

--- apis/userprofile/server.js
+++ apis/userprofile/server.js
@@ -36,7 +36,7 @@ App.use(Swaggerize({
 
 App.use('/api/docs/user', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// comment 2
+// comment 3
 Server.listen(8080, function () {
     App.swagger.api.host = this.address().address + ':' + this.address().port;
     /* eslint-disable no-console */
